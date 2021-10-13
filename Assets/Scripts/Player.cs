53e53ba9129b20f38f3c1c2adc90edfc94cfd04e
Uksus1147 <tihomirovdima028@gmail.com>
2021-10-13 21:37:50 +0300

    Домашнее задание к уроку4 Тихомирова Дмитрия

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Vector3 _direct;      
+    public float speed = 2;
+    public float Rotatespeed = 100f;
+    //private GameObject _bulletPrefab;
+    //public Transform _spawnBulletPoint;
+    //private float damage = 2f;
+    
+   
+
+    private void Awake()
+    {
+        _direct = Vector3.zero;
+    }
+    private void Update()
+    {
+        _direct.x = Input.GetAxis("Horizontal");
+        _direct.z = Input.GetAxis("Vertical");
+    }
+    private void FixedUpdate()
+    {
+        Move();
+        //Fire();
+
+        transform.Rotate(0, Input.GetAxis("Mouse X") * Rotatespeed * Time.fixedDeltaTime, 0);
+        
+
+    }
+    private void Move()
+    {
+        Vector3 direction = _direct * speed * Time.fixedDeltaTime;
+        transform.Translate(direction);
+    }
+    //private void Fire()
+   // {
+    //    GameObject bulletObject = Instantiate(_bulletPrefab, _spawnBulletPoint.position, _spawnBulletPoint.rotation);
+   //     Bullet bullet = bulletObject.transform.gameObject.GetComponent<Bullet>();
+    //    bullet.Initialization(damage, 3f, null);  
+    //}
+}
---
+++ Assets/Scripts/enemyPeople.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemyPeople : MonoBehaviour
+{
+    public Player player;
+    public Transform dulo;
+    public NavMeshAgent _agent;
+    public Transform[] wyapoints;
+    public int wyapointid;
+    private void Awake()
+    {
+        player = GameObject.FindObjectOfType<Player>();
+        _agent = GetComponent<NavMeshAgent>();
+    }
+    void Start()
+    {
+        _agent.SetDestination(wyapoints[0].position);
+    }
+
+    void Update()
+    {
+        if (_agent.remainingDistance < _agent.stoppingDistance)
+        {
+            wyapointid = (wyapointid + 1) % wyapoints.Length;
+            _agent.SetDestination(wyapoints[wyapointid].position);
+        }
+        Vector3 direction = player.transform.position - dulo.position;
+        Vector3 forwardDirection = new Vector3(direction.x, 0, direction.z);
+        dulo.rotation = Quaternion.LookRotation(forwardDirection);
+    }
+
+}
